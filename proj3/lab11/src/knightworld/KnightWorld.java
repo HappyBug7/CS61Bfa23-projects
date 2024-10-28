6328e38bd36eebda8e1ead08ba73a2ee53af3210
HappyBug <fubowen2004@outlook.com>
2024-10-28 18:23:03 +0800

    finished knight's world

--- proj3/lab11/src/knightworld/KnightWorld.java
+++ proj3/lab11/src/knightworld/KnightWorld.java
@@ -1,25 +1,97 @@
 package knightworld;
 
+import org.apache.hc.core5.annotation.Internal;
 import tileengine.TERenderer;
 import tileengine.TETile;
+import tileengine.Tileset;
+
+import java.util.Random;
 
 /**
  * Draws a world consisting of knight-move holes.
  */
 public class KnightWorld {
 
-    private TETile[][] tiles;
-    // TODO: Add additional instance variables here
+    private final TETile[][] tiles;
+    private final int WIDTH;
+    private final int HEIGHT;
+    private static final long SEED = 2873123;
+    private static final Random RANDOM = new Random(SEED);
 
     public KnightWorld(int width, int height, int holeSize) {
-        // TODO: Fill in this constructor and class, adding helper methods and/or classes as necessary to draw the
-        //  specified pattern of the given hole size for a window of size width x height. If you're stuck on how to
-        //  begin, look at the provided demo code!
+        WIDTH = width;
+        HEIGHT = height;
+        tiles = new TETile[WIDTH][HEIGHT];
+        fillAll();
+        int randomPointX = RANDOM.nextInt(WIDTH);
+        int randomPointY = RANDOM.nextInt(HEIGHT);
+        digHoleRecursive(randomPointX, randomPointY, holeSize);
     }
 
     /** Returns the tiles associated with this KnightWorld. */
     public TETile[][] getTiles() {
         return tiles;
+    }
+
+    public void digHoleRecursive(int startPointX, int startPointY, int size) {
+        if (startPointX + size - 1 < 0 || startPointY + size - 1 < 0 || startPointX >= WIDTH || startPointY >= HEIGHT) {
+            return;
+        }
+        int checkPointX;
+        int checkPointY;
+        if (startPointX < 0) {
+            checkPointX = startPointX + size - 1;
+        } else {
+            checkPointX = startPointX;
+        }
+        if (startPointY < 0) {
+            checkPointY = startPointY + size - 1;
+        } else {
+            checkPointY = startPointY;
+        }
+        if (tiles[checkPointX][checkPointY] == Tileset.NOTHING) {
+            return;
+        }
+
+        digHole(startPointX, startPointY, size);
+
+        // left top
+        int newStartPointX = startPointX - 2 * size;
+        int newStartPointY = startPointY + size;
+        digHoleRecursive(newStartPointX, newStartPointY, size);
+
+        // right top
+        newStartPointX = startPointX + size;
+        newStartPointY = startPointY + 2 * size;
+        digHoleRecursive(newStartPointX, newStartPointY, size);
+
+        // right bottom
+        newStartPointX = startPointX + 2 * size;
+        newStartPointY = startPointY - size;
+        digHoleRecursive(newStartPointX, newStartPointY, size);
+
+        // left bottom
+        newStartPointX = startPointX - size;
+        newStartPointY = startPointY - 2 * size;
+        digHoleRecursive(newStartPointX, newStartPointY, size);
+    }
+
+    public void digHole(int startPointX, int startPointY, int size) {
+        for (int x = startPointX; x <= startPointX + size - 1; x++) {
+            for (int y = startPointY; y <= startPointY + size - 1; y++) {
+                if (x >= 0 && x < WIDTH && y >= 0 && y < HEIGHT) {
+                    tiles[x][y] = Tileset.NOTHING;
+                }
+            }
+        }
+    }
+
+    public void fillAll() {
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                tiles[x][y] = Tileset.LOCKED_DOOR;
+            }
+        }
     }
 
     public static void main(String[] args) {
